811946a64a2eacbd7a195e1f9fb06e8ba906412e
Adrien Fallou <adrien.fallou@gmail.com>
2016-11-11 12:43:57 -0800

    Stringify pagination argument values.
    This change allows custom types (`Enum`, `UUID`) to "do the right thing".

--- microcosm_flask/tests/test_paging.py
+++ microcosm_flask/tests/test_paging.py
@@ -2,13 +2,16 @@
 Paging tests.
 
 """
+from enum import Enum, unique
+from uuid import uuid4
+
 from hamcrest import (
     assert_that,
     equal_to,
     is_,
 )
-
 from microcosm.api import create_object_graph
+
 from microcosm_flask.conventions.encoding import load_query_string_data
 from microcosm_flask.namespaces import Namespace
 from microcosm_flask.operations import Operation
@@ -118,6 +121,15 @@ def test_paginated_list_relation_to_dict():
         })))
 
 
+@unique
+class MyEnum(Enum):
+    ONE = u"ONE"
+    TWO = u"TWO"
+
+    def __str__(self):
+        return self.name
+
+
 def test_custom_paginated_list():
     graph = create_object_graph(name="example", testing=True)
     ns = Namespace(subject="foo", object_="bar")
@@ -126,14 +138,17 @@ def test_custom_paginated_list():
     def search_foo():
         pass
 
+    uid = uuid4()
     paginated_list = PaginatedList(
         ns,
-        Page.from_query_string(dict(offset=2, limit=2, baz="baz")),
+        Page.from_query_string(dict(offset=2, limit=2, baz="baz", uid=uid, value=MyEnum.ONE)),
         ["1", "2"],
         10,
         operation=Operation.SearchFor,
         foo_id="FOO_ID",
     )
+
+    rest = "baz=baz&uid={}&value=ONE".format(uid)
 
     with graph.flask.test_request_context():
         assert_that(paginated_list.to_dict(), is_(equal_to({
@@ -146,14 +161,16 @@ def test_custom_paginated_list():
             "limit": 2,
             "_links": {
                 "self": {
-                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=2&limit=2&baz=baz",
+                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=2&limit=2&{}".format(rest),
                 },
                 "next": {
-                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=4&limit=2&baz=baz",
+                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=4&limit=2&{}".format(rest),
                 },
                 "prev": {
-                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=0&limit=2&baz=baz",
+                    "href": "http://localhost/api/foo/FOO_ID/bar?offset=0&limit=2&{}".format(rest),
                 },
             },
             "baz": "baz",
+            "uid": str(uid),
+            "value": "ONE",
         })))
