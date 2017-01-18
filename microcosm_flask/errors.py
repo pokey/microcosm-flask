172ec4360c1e6fed9448ecd229eaa5718eb8d48e
Pokey Rule <pokey.rule@gmail.com>
2017-01-18 17:40:48 +0000

    Hack to fix error reporting

--- microcosm_flask/errors.py
+++ microcosm_flask/errors.py
@@ -125,7 +125,7 @@ def make_json_error(error):
     # Serialize into JSON response
     response_data = {
         "code": status_code,
-        "context": context,
+        "context": str(context),
         "message": message,
         "retryable": retryable,
     }
